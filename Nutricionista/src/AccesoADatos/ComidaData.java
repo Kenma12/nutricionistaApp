c14ca8fd8bf9620f3e2d706b852acd5a71a3b8a2
Enzo Ezequiel Ugarte <enzougarte123@gmail.com>
2023-10-19 19:24:05 -0300

    bajaComida

--- Nutricionista/src/AccesoADatos/ComidaData.java
+++ Nutricionista/src/AccesoADatos/ComidaData.java
@@ -8,6 +8,11 @@ import entidades.Comida;
 import org.mariadb.jdbc.Connection;
 import java.sql.PreparedStatement;
 import org.mariadb.jdbc.Statement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -16,8 +21,7 @@ import org.mariadb.jdbc.Statement;
 public class ComidaData {
     
     private Connection conexion;
-    private RegistroPesoData registroData = new RegistroPesoData();
-    
+ 
     public ComidaData(){
         conexion = Conexion.getConnection();
     }
@@ -25,55 +29,48 @@ public class ComidaData {
     public void altaComida(Comida comida){
         String sql = "INSERT INTO `comida`(`nombreComida`, `detalle`, `cantCalorias`) "
                 + "VALUES (?, ?, ?)";
-        
         try{
-            
             PreparedStatement ps = conexion.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             
             ps.setString(1, comida.getNombreComida());
             ps.setString(2, comida.getDetalle());
             ps.setInt(3,comida.getCantCalorias());
-            
-            
-        }catch(SQLException ex){
-            
-        }
-        
-        
-        
-        
-        
-    }
-    
-    /*
-    public void altaPaciente(Paciente paciente){
-        String sql = "INSERT INTO `paciente`(`nombrePaciente`, `dni`, `domicilio`, `telefono`, `pesoActual`, `pesoDeseado`) "
-                    + "VALUES (?, ?, ?, ?, ?, ?)";
-    
-        try {  
-            PreparedStatement ps = conexion.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
-           
-            ps.setString(1, paciente.getNombrePaciente());
-            ps.setInt(2, paciente.getDni());
-            ps.setString(3, paciente.getDomicilio());
-            ps.setString(4, paciente.getTelefono());
-            ps.setDouble(5, paciente.getPesoActual());
-            ps.setDouble(6, paciente.getPesoDeseado());
-            
             ps.executeUpdate();
-            ResultSet rs = ps.getGeneratedKeys(); 
-            
+            ResultSet rs = ps.getGeneratedKeys();
             if (rs.next()){
-                paciente.setIdPaciente(rs.getInt(1));
-                JOptionPane.showMessageDialog(null, "Paciente agregado.");
+                comida.setIdComida(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "COMIDA AGREGADA");
             }
             rs.close();
             ps.close();
-        } catch (SQLException ex) {
+        }catch(SQLException ex){
             JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
         }
     }
-    */
+    
+    public void bajaComida(int id){
+        
+        String sql = "DELETE FROM `comida` WHERE idComida = ?";
+        
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ps.setInt(1, id);
+            int e = ps.executeUpdate();
+            
+            if (e == 1){
+                JOptionPane.showMessageDialog(null, "Comida borrada.");
+            }else{
+                JOptionPane.showMessageDialog(null, "Comida no encontrada.");
+            }
+          
+            
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
+        }
+        
+        
+        
+    }
     
     
     
