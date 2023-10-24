8fe1ed27f06baf9f41d206c1a316a00419180ff4
BerDiaz01 <berdiaz01@hotmail.com>
2023-10-24 00:01:50 -0300

    Update DietaData.java
    hay que arreglar algunas lineas porque en la entidad aparecen en mayusculas y tira error

--- Nutricionista/src/AccesoADatos/DietaData.java
+++ Nutricionista/src/AccesoADatos/DietaData.java
@@ -5,12 +5,14 @@
 package AccesoADatos;
 
 import entidades.dieta;
+import java.sql.Date;
 import org.mariadb.jdbc.Connection;
 import java.sql.PreparedStatement;
 import org.mariadb.jdbc.Statement;
 import java.sql.ResultSet;
 import javax.swing.JOptionPane;
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -30,9 +32,9 @@ public class DietaData {
             /* Paciente paciente, LocalDate fechaInicial, LocalDate fechaFinal, double pesoInicial, double pesoFinal)*/
             PreparedStatement ps = conexion.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ps.setString(1, dieta.getNombreDieta());
-           // ps.setInt(2, dieta.getPaciente());
-            //ps.setInt(3,dieta.getFechaInicial());
-           // ps.setInt(4,dieta.getFechaFinal());
+            //ps.setPaciente(2, dieta.getPaciente());
+            ps.setDate(3, Date.valueOf(dieta.getFechaInicial()));
+            ps.setDate(4,Date.valueOf(dieta.getFechaFinal()));
             ps.setDouble(5,dieta.getPesoInicial());
             ps.setDouble(6,dieta.getPesoFinal());
             ps.executeUpdate();
@@ -65,4 +67,35 @@ public class DietaData {
             JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
         }
     }
+    public void modificarDietaData(dieta dieta){
+        String sql = "UPDATE `dieta` "
+                + "SET `nombreDieta`=?,`idPaciente`=?,`fechaInicial`=? "
+                + "`fechaFinal`= ?,`pesoInicial`=?,`pesoFinal`=?"
+                + "WHERE idDieta = ?;";
+         /*  WHERE 1*/      
+        try {
+            /*no sirve aca 
+            //Guarda su peso anterior en un registroPeso
+            Paciente aux = buscarPacienteXId(paciente.getIdPaciente());
+            RegistroPeso registro = new RegistroPeso(aux, aux.getPesoActual()); 
+            registroData.nuevoRegistro(registro);*/
+            
+            //ejecuta el cambio de peso
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ps.setString(1, dieta.getNombreDieta());
+           // ps.setPaciente(2, dieta.getPaciente());
+            ps.setDate(3, Date.valueOf(dieta.getFechaInicial()));
+            ps.setDate(4, Date.valueOf(dieta.getFechaFinal()));
+            ps.setDouble(5, dieta.getPesoInicial());
+            ps.setDouble(6, dieta.getPesoFinal());
+            ps.setInt(7, dieta.getIdDieta());
+            int ex = ps.executeUpdate();
+            if(ex == 1){
+                JOptionPane.showMessageDialog(null, "La dieta se ha modificado.");
+            }
+        } catch (SQLException ex) {
+             JOptionPane.showMessageDialog(null, "Error: " + ex.getMessage());
+        }
+    } 
 }
+
